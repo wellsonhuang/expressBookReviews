242308011dcef3292bde15b53cf8e74a82c2882f
wellsonhuang <wellsonhuang888@gmail.com>
2025-06-30 13:09:42 -0400

    first

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,43 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const bookId = parseInt(isbn);
+
+  if (books[bookId]) {
+    return res.status(200).json(books[bookId]);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const matchingBooks = [];
+  
+    // 取得所有 book key
+    const bookIds = Object.keys(books);
+  
+    // 遍歷所有書
+    bookIds.forEach(id => {
+      if (books[id].author.toLowerCase() === author.toLowerCase()) {
+        matchingBooks.push(books[id]);
+      }
+    });
+  
+    if (matchingBooks.length > 0) {
+      return res.status(200).json(matchingBooks);
+    } else {
+      return res.status(404).json({ message: "No books found for this author" });
+    }
 });
 
 // Get all books based on title
