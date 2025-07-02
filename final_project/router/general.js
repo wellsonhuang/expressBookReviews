59f7815f8deb6afed80c6bb40d6a6d4c507d2d00
wellsonhuang <wellsonhuang888@gmail.com>
2025-07-02 04:29:58 -0400

    second

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,21 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const{username, password}=req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+}
+// 檢查是否已經存在相同 username
+const userExists = users.find(user => user.username === username);
+
+if (userExists) {
+    return res.status(409).json({ message: "Username already exists" });
+}
+
+// 寫入新使用者
+users.push({ username, password });
+
+return res.status(200).json({ message: "User registered successfully" });
 });
 
 // Get the book list available in the shop
@@ -41,7 +54,7 @@ public_users.get('/author/:author',function (req, res) {
     // 遍歷所有書
     bookIds.forEach(id => {
       if (books[id].author.toLowerCase() === author.toLowerCase()) {
-        matchingBooks.push(books[id]);
+        matchingBooks.push({ isbn: id, ...books[id] });
       }
     });
   
@@ -55,13 +68,34 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const matchingBooks = [];
+
+  for (let key in books){
+    if (books[key].title.toLowerCase() === title.toLowerCase()){
+        matchingBooks.push({isbn: key, ... books[key]});
+    }
+    if (matchingBooks.length>0){
+        return res.status(200).json(matchingBooks);
+    }else{
+        return res.status(404).json({ message: "No books found with the given title" });
+    }
+  }
+
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+    // 檢查是否存在這本書
+    if (books[isbn]) {
+        const reviews = books[isbn].reviews;
+        return res.status(200).json(reviews);
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
 });
 
 module.exports.general = public_users;
